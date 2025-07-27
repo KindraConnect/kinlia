90f9fd6b6469cec4f92e8adfc072d9f044aaa656
Allie Ray Jones <alliejones42@gmail.com>
2025-07-27 15:57:39 -0700

    Add Pinecone matching endpoint

---
+++ backend/app/pinecone_client.py
@@ -0,0 +1,13 @@
+import os
+import pinecone
+
+api_key = os.getenv("PINECONE_API_KEY")
+# Pinecone environment and index can optionally be specified via env vars
+environment = os.getenv("PINECONE_ENVIRONMENT", "us-east-1")
+index_name = os.getenv("PINECONE_INDEX", "kinlia")
+
+if api_key:
+    pinecone.init(api_key=api_key, environment=environment)
+    index = pinecone.Index(index_name)
+else:
+    index = None
